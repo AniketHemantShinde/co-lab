a734d80917ccda78ca3b31f63b343895949c199d
Aniket Hemant Shinde <aniketshinde.work@gmail.com>
2018-09-15 14:32:55 +0530

    Done with the space module

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); 
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://localhost:27017/petShop');
+mongoose.connection.on('error',function(){
+    console.log('error in mongo connection');
+})
+mongoose.connection.on('open',function(){
+    console.log('connected to mongo');
+})
+
+const spaceController = require('./controllers/space');
+
 
 const app = express();
 
@@ -10,6 +23,14 @@ app.use(bodyParser.urlencoded(
     }
 ))
 
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+
+app.get('/api/v1/spaces', spaceController.getAllSpaces)
+app.post('/api/v1/spaces', spaceController.postNewSpaces)
+app.put('/api/v1/spaces/:id', spaceController.updateSpacesById)
+app.delete('/api/v1/spaces/:id', spaceController.delSpacesById)
 
 
 
